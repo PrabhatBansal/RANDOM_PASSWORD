eca209013464a8b7bade9d8942518a07c0726ab7
PrabhatBansal <96734004+PrabhatBansal@users.noreply.github.com>
2022-03-05 13:23:18 +0530

    Update save.js

--- save.js
+++ save.js
@@ -59,7 +59,7 @@ function copy(e) {
 
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function () {
-  let inputVal = search.value.toLowerCase();
+  let inputVal = search.value;
   // console.log('Input event fired!', inputVal);
   let noteCards = document.getElementsByClassName("noteCard");
   Array.from(noteCards).forEach(function (element) {
